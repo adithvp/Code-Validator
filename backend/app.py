1af6d68c34cf4f62303065be5ad4df6e53565c9f
adithvp <adithvp318@gmail.com>
2026-06-07 17:22:31 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -49,4 +49,4 @@ app.include_router(validate_router)
 
 
 if __name__ == "__main__":
-    uvicorn.run("app:app", host="127.0.0.1", port=8000, reload=False)
+    uvicorn.run("app:app", host="host_no", port=portno, reload=False)
